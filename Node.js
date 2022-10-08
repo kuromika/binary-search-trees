cb251138516f39edd4a5990ba9120597d5eb9532
mika-alpha <yuukeikioku@gmail.com>
2022-10-08 13:02:32 -0500

    Create Node class

---
+++ Node.js
@@ -0,0 +1,25 @@
+
+class Node{
+
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    get left() {
+        return this._left;
+    }
+
+    get right() {
+        return this._right;
+    }
+
+    set left(data) {
+        this._left = data;
+    }
+
+    set right(data) {
+        this._right = data;
+    }
+}
